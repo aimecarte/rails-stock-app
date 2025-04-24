374d80b585f3771380c0dd8e100a5d0fbbf19d55
Aga Mecarte <aga.mecarte@outlook.com>
2025-04-24 20:41:51 +0800

    added additional api queries

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,15 @@
+class StocksController < ApplicationController
+  def index
+    if params[:symbol]
+      response = AvaApi.fetch_records(params[:symbol])
+      @symbol = response["Global Quote"]["01. symbol"]
+      @stock_price = response["Global Quote"]["05. price"]
+      binding.b
+    end
+  end
+
+  def search
+    response = AvaApi.search(params[:query])
+    @results = response["bestMatches"].map { |e| { symbol: e["1. symbol"], name: e["2. name"] } }
+  end
+end
