579ed3a338985230217edc72613fc9357f5591f0
Aga Mecarte <aga.mecarte@outlook.com>
2025-04-25 20:11:25 +0800

    added adding of stocks on view

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,7 +4,6 @@ class StocksController < ApplicationController
       response = AvaApi.fetch_records(params[:symbol])
       @symbol = response["Global Quote"]["01. symbol"]
       @stock_price = response["Global Quote"]["05. price"]
-      binding.b
     end
   end
 
@@ -12,4 +11,6 @@ class StocksController < ApplicationController
     response = AvaApi.search(params[:query])
     @results = response["bestMatches"].map { |e| { symbol: e["1. symbol"], name: e["2. name"] } }
   end
+
+  def create; end
 end
