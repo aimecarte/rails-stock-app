36ca5c51ac2ed7cd143cb8543645b0773201b895
jhunestimada <jhunestimada@gmail.com>
2025-04-25 12:51:06 +0800

    Added some features to admin. Can now view admin pages as admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get "home/index"
+  get "admin/index"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,4 +16,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "stocks#index"
   get "/search", to: "stocks#search"
+
+
+
+  resources :admin
+  
 end
